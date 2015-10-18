1b8c7f7f3be84552c76702bd6c6a62aec510f1a1
Frank Chan <fc249@cornell.edu>
2015-10-18 14:32:31 -0400

    reinitial commit

---
+++ scripts.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+  var changeNav = $('#middle').offset().top;
+  $navbar = $('.navbar.navbar-default.navbar-fixed-top')
+  $(document).scroll(function() {
+    if($(window).scrollTop() >= changeNav) {
+      $navbar.css('background-color', "#84939A");
+      $navbar.css('padding', "1% 1% 1% 1%");
+      $('.nav.navbar-nav li a').css('font-weight', "300");
+    }
+    else {
+      $navbar.css('background-color', 'transparent');
+      $navbar.css('padding', "2% 1% 0px 0px");
+      $('.nav.navbar-nav li a').css('font-weight', "700");
+    }
+  });
+});
